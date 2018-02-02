da74d020956e6a17032fcc3f7f31d5c0b63eb40d
fcrh <coquelicot1117@gmail.com>
2018-02-02 03:30:23 -0500

    Don't know what's going on

--- src/cpp/include/runtime.hpp
+++ src/cpp/include/runtime.hpp
@@ -1,29 +1,123 @@
 #ifndef __RUNTIME_H__
 #define __RUNTIME_H__
 
-#include <memory>
+#include <map>
+#include <array>
+#include <iostream>
 
 struct lmb_t {
-    virtual std::shared_ptr<lmb_t> exec(std::shared_ptr<lmb_t>) = 0;
-    virtual ~lmb_t() {};
+    lmb_t *(*exec)(lmb_t *env[], lmb_t *arg);
+    unsigned long ref_cnt;
+    lmb_t *envs[0];
 };
 
-struct __builtin_p0_t : public lmb_t {
-    std::shared_ptr<lmb_t> exec(std::shared_ptr<lmb_t>);
-};
+template <int id, int n>
+static lmb_t *create(std::array<long, n+3>&& ref) {
 
-struct __builtin_p1_t : public lmb_t {
-    std::shared_ptr<lmb_t> exec(std::shared_ptr<lmb_t>);
-};
+    std::map<std::array<long, n+3>, lmb_t*> cache;
 
-struct __builtin_g_t : public lmb_t {
-    std::shared_ptr<lmb_t> exec(std::shared_ptr<lmb_t>);
-};
+    auto it = cache.find(ref);
+    if (it != cache.end()) {
+        ++it->second->ref_cnt;
+        return it->second;
+    } else {
+        return cache[ref] = (lmb_t*)(new std::array<long, n+3>(ref));
+    }
+}
 
-extern std::shared_ptr<lmb_t> __builtin_g;
-extern std::shared_ptr<lmb_t> __builtin_p0;
-extern std::shared_ptr<lmb_t> __builtin_p1;
+static bool __is_pure = true;
 
-std::shared_ptr<lmb_t> apply(std::shared_ptr<lmb_t> a, std::shared_ptr<lmb_t> b);
+static inline void output(int bit) {
+
+    static int pos = 7;
+    static int val = 0;
+
+    val |= (bit << pos--);
+    if (pos < 0) {
+        std::cout << char(val);
+        std::cout.flush();
+        pos = 7, val = 0;
+    }
+
+    __is_pure = false;
+}
+
+static inline int input() {
+
+    // FIXME: how about eof?
+
+    static int pos = EOF;
+    static int val = 0;
+
+    if (pos < 0) {
+        val = std::cin.get();
+        if (val == EOF)
+            return 1;
+        pos = 7;
+    }
+
+    __is_pure = false;
+    return (val >> pos--) & 1;
+}
+
+static lmb_t *__do_builtin_p0(lmb_t **, lmb_t *w) {
+    output(0);
+    ++w->ref_cnt;
+    return w;
+}
+
+static lmb_t *__do_builtin_p1(lmb_t **, lmb_t *w) {
+    output(1);
+    ++w->ref_cnt;
+    return w;
+}
+
+static lmb_t *__do_builtin_g2(lmb_t **e, lmb_t *c) {
+    int b = input();
+    lmb_t *retv = (b == EOF) ? c : e[b];
+    ++retv->ref_cnt;
+    return retv;
+}
+
+static lmb_t *__do_builtin_g1(lmb_t **e, lmb_t *a1) {
+    return create<-1, 2>({{(long)__do_builtin_g2, 1, (long)e[0], (long)a1, 0}});
+}
+
+static lmb_t *__do_builtin_g(lmb_t **, lmb_t *a0) {
+    return create<-1, 1>({{(long)__do_builtin_g1, 1, (long)a0, 0}});
+}
+
+static inline void release(struct lmb_t *lmb) {
+    if (!--lmb->ref_cnt)
+        for (int i = 0; lmb->envs[i]; i++)
+            release(lmb->envs[i]);
+}
+
+static inline lmb_t *apply(lmb_t *a, lmb_t *b) {
+
+    static std::map<std::pair<lmb_t*, lmb_t*>, lmb_t*> caches;
+
+    auto key = std::make_pair(a, b);
+    auto it = caches.find(key);
+    if (it != caches.end())
+        return it->second;
+
+    bool __ref_pure = __is_pure;
+    __is_pure = true;
+
+    struct lmb_t *retv = a->exec(a->envs, b);
+    if (__is_pure) {
+        caches[key] = retv;
+        ++retv->ref_cnt;
+        __is_pure = __ref_pure;
+    } else {
+        __is_pure = false;
+    }
+    return retv;
+}
+
+static lmb_t *__builtin_g(create<-1, 0>({{(long)__do_builtin_g, 1, 0}}));
+static lmb_t *__builtin_p0(create<-1, 0>({{(long)__do_builtin_p0, 1, 0}}));
+static lmb_t *__builtin_p1(create<-1, 0>({{(long)__do_builtin_p1, 1, 0}}));
 
 #endif
--- src/cpp/lib/transpiler.cpp
+++ src/cpp/lib/transpiler.cpp
@@ -31,7 +31,7 @@ struct code_id_t {
             case code_id_t::LOCAL:
                 return stm << "_" << id.val;
             case code_id_t::ENV:
-                return stm << "_e" << id.val;
+                return stm << "_e[" << id.val << "]";
             case code_id_t::ARG:
                 return stm << "_a";
             default:
@@ -63,24 +63,24 @@ struct code_block_t {
 };
 
 struct code_lmb_t {
+
     code_id_t name;
-    code_id_t obj;
     int env_cnt;
     code_block_t body;
     int next_local_id;
-    bool emited;
 };
 
 struct transpiler_t::impl_t {
 
     int global_id;
     std::map<std::string, code_id_t> builtins;
-    std::map<std::string, int> ident_cnt;
-    std::map<code_id_t, std::shared_ptr<code_lmb_t>> static_lmbs;
+    std::set<code_id_t> builtin_ids;
 
     impl_t(transpiler_t *parent) : global_id(0) {
         for (auto str : parent->builtins)
             builtins.insert(std::make_pair(str, next_global_id()));
+        for (auto pair : builtins)
+            builtin_ids.insert(pair.second);
     }
 
     code_id_t next_global_id() {
@@ -105,98 +105,73 @@ struct transpiler_t::impl_t {
 
     void transpile(node_hdr_t node, std::ostream &stm) {
 
-        ident_cnt.clear();
         global_id = builtins.size();
+        std::map<std::string, int> ident_cnt;
         std::vector<code_inst_t> insts;
         std::map<std::string, code_id_t> envs;
         std::set<std::shared_ptr<code_lmb_t>> deps;
 
         code_id_t name = next_global_id();
-        code_id_t obj = next_global_id();
         code_id_t retv = local_id(0);
         int next_local_id = 1;
         int next_env_id = 0;
-        __transpile(node, next_local_id, next_env_id, retv, insts, envs, deps);
+        __transpile(node, next_local_id, next_env_id, retv, insts, envs, deps, ident_cnt);
 
         code_block_t block{retv, insts, deps};
-        std::shared_ptr<code_lmb_t> prog(new code_lmb_t{name, obj, 0, block, next_local_id, false});
+        std::shared_ptr<code_lmb_t> prog(new code_lmb_t{name, 0, block, next_local_id});
 
         // optimize
         __inline_temp_lmb(prog);
         __extract_static_lmb(prog); // this assumes that all temp lmb are inlined
+        __dedup_lmb(prog);
 
         // output
-        stm << "#include \"runtime.hpp\"\n";
+        __emit(prog, stm);
+    }
 
+    void __emit(std::shared_ptr<code_lmb_t> lmb, std::ostream &stm) {
+
+        stm << "#include \"runtime.hpp\"\n";
         for (auto pair : builtins)
             stm << "auto& " << pair.second << " = " << pair.first << ";\n";
 
-        __emit(*prog, stm);
+        std::set<std::shared_ptr<code_lmb_t>> emited;
+        __emit(lmb, stm, emited);
+
         stm << "int main() {\n"
-            << "  " << obj << "->exec(nullptr);\n"
+            << "  " << lmb->name << "->exec(0x0, 0x0);\n"
             << "}\n";
     }
 
-    void __emit(code_lmb_t &lmb, std::ostream &stm) {
+    void __emit(std::shared_ptr<code_lmb_t> lmb, std::ostream &stm, std::set<std::shared_ptr<code_lmb_t>> &emited) {
 
-        if (lmb.emited)
+        if (emited.count(lmb))
             return;
-        lmb.emited = true;
+        emited.insert(lmb);
 
-        for (auto dep : lmb.body.deps)
-            __emit(*dep, stm);
+        for (auto dep : lmb->body.deps)
+            __emit(dep, stm, emited);
 
-        // header
-        stm << "struct " << lmb.name << " : public lmb_t {\n";
-
-        // eval
-        stm << "  std::shared_ptr<lmb_t> exec(std::shared_ptr<lmb_t> " << arg_id() << ") {\n";
-        for (auto inst : lmb.body.insts) {
-            stm << "    auto " << inst.retv << " = ";
+        // FIXME: hard coded var name
+        stm << "lmb_t *" << lmb->name << "_t(lmb_t *_e[], lmb_t *" << arg_id() << ") {\n";
+        for (auto inst : lmb->body.insts) {
+            stm << "    lmb_t *" << inst.retv << " = ";
             if (inst.type == code_inst_t::APPLY) {
                 stm << "apply(" << inst.func << ", " << inst.arg << ");\n";
             } else {
-                stm << "std::make_shared<" << inst.lmb->name << ">(";
-                for (auto it = inst.envs.begin(); it != inst.envs.end(); it++) {
-                    if (it != inst.envs.begin())
-                        stm << ", ";
-                    stm << *it;
-                }
-                stm << ");\n";
+                stm << "create<" << inst.lmb->name.val << ", " << inst.envs.size() << ">({{"
+                    << "(long)" << inst.lmb->name << "_t, 1";
+                for (auto it = inst.envs.begin(); it != inst.envs.end(); it++)
+                    stm << ", (long)" << *it;
+                stm << ", 0}});\n";
             }
         }
-        stm << "    return " << lmb.body.retv << ";\n";
-        stm << "  };\n";
-
-        // constructor
-        stm << "  " << lmb.name << "(";
-        for (int i = 0; i < lmb.env_cnt; i++) {
-            if (i != 0)
-                stm << ", ";
-            stm << "std::shared_ptr<lmb_t> _" << env_id(i);
-        }
-        stm << ")";
-        for (int i = 0; i < lmb.env_cnt; i++) {
-            stm << (i != 0 ? ", " : " : ");
-            stm << env_id(i) << "(_" << env_id(i) << ")";
-        }
-        stm << " {}\n";
-
-        // vars
-        if (lmb.env_cnt > 0) {
-            stm << "  std::shared_ptr<lmb_t> ";
-            for (int i = 0; i < lmb.env_cnt; i++) {
-                if (i != 0)
-                    stm << ", ";
-                stm << env_id(i);
-            }
-            stm << ";\n";
-        }
-
-        // end
+        stm << "    return " << lmb->body.retv << ";\n";
         stm << "};\n";
-        if (lmb.obj.type != code_id_t::NONE)
-            stm << "std::shared_ptr<lmb_t> " << lmb.obj << "(new " << lmb.name << "());\n";
+
+        // static obj
+        if (lmb->env_cnt == 0)
+            stm << "lmb_t *" << lmb->name << "(create<" << lmb->name.val << ", 0>({{(long)" << lmb->name << "_t, 1, 0}}));\n";
     }
 
     void __transpile(
@@ -206,8 +181,9 @@ struct transpiler_t::impl_t {
         code_id_t &retv,
         std::vector<code_inst_t> &insts,
         std::map<std::string, code_id_t> &envs,
-        std::set<std::shared_ptr<code_lmb_t>> &deps)
-    {
+        std::set<std::shared_ptr<code_lmb_t>> &deps,
+        std::map<std::string, int> &ident_cnt
+    ) {
 
         try {
             term_node_t &node = dynamic_cast<term_node_t&>(*_node);
@@ -229,8 +205,8 @@ struct transpiler_t::impl_t {
 
             code_id_t func = local_id(next_local_id++);
             code_id_t arg = local_id(next_local_id++);
-            __transpile(node.nd_fun, next_local_id, next_env_id, func, insts, envs, deps);
-            __transpile(node.nd_arg, next_local_id, next_env_id, arg, insts, envs, deps);
+            __transpile(node.nd_fun, next_local_id, next_env_id, func, insts, envs, deps, ident_cnt);
+            __transpile(node.nd_arg, next_local_id, next_env_id, arg, insts, envs, deps, ident_cnt);
             insts.push_back(code_inst_t{code_inst_t::APPLY, retv, func, arg, nullptr, {}});
 
             return;
@@ -249,12 +225,12 @@ struct transpiler_t::impl_t {
 
             ident_cnt[node.arg]++;
             lmb_envs[node.arg] = arg_id();
-            __transpile(node.nd_body, lmb_next_local_id, lmb_next_env_id, lmb_retv, lmb_insts, lmb_envs, lmb_deps);
+            __transpile(node.nd_body, lmb_next_local_id, lmb_next_env_id, lmb_retv, lmb_insts, lmb_envs, lmb_deps, ident_cnt);
             ident_cnt[node.arg]--;
             lmb_envs.erase(node.arg);
 
             code_block_t block{lmb_retv, lmb_insts, lmb_deps};
-            std::shared_ptr<code_lmb_t> lmb(new code_lmb_t{name, none_id(), (int)lmb_envs.size(), block, lmb_next_local_id, false});
+            std::shared_ptr<code_lmb_t> lmb(new code_lmb_t{name, (int)lmb_envs.size(), block, lmb_next_local_id});
             deps.insert(lmb);
 
             std::vector<code_id_t> lmb_venvs(lmb_envs.size());
@@ -298,13 +274,10 @@ struct transpiler_t::impl_t {
                 inst.lmb->env_cnt = nenvs.size();
 
                 // it's static!! make an alias
-                if (nenvs.empty()) {
+                if (nenvs.empty())
                     alias[inst.retv] = inst.lmb;
-                    assert(inst.lmb->obj.type == code_id_t::NONE);
-                    inst.lmb->obj = next_global_id();
-                } else {
+                else
                     ninsts.push_back(inst);
-                }
 
                 // extract recursively no matter what
                 __extract_static_lmb(inst.lmb, lmb_alias, lmb_env_map);
@@ -313,11 +286,11 @@ struct transpiler_t::impl_t {
             }
 
             if (alias.count(inst.func))
-                inst.func = alias[inst.func]->obj;
+                inst.func = alias[inst.func]->name;
             else if (env_map.count(inst.func))
                 inst.func = env_map.at(inst.func);
             if (alias.count(inst.arg))
-                inst.arg = alias[inst.arg]->obj;
+                inst.arg = alias[inst.arg]->name;
             else if (env_map.count(inst.arg))
                 inst.arg = env_map.at(inst.arg);
             ninsts.push_back(inst);
@@ -325,7 +298,7 @@ struct transpiler_t::impl_t {
 
         lmb->body.insts = ninsts;
         if (alias.count(lmb->body.retv))
-            lmb->body.retv = alias[lmb->body.retv]->obj;
+            lmb->body.retv = alias[lmb->body.retv]->name;
         for (auto &pair : alias)
             lmb->body.deps.insert(pair.second);
     }
@@ -415,7 +388,112 @@ struct transpiler_t::impl_t {
         }
     }
 
-    void __inline_static_lmb(std::shared_ptr<code_lmb_t> lmb) {
+    typedef std::pair<std::shared_ptr<code_lmb_t>, std::vector<int>> code_lmb_ref_t;
+    typedef std::pair<std::string, std::vector<int>> code_lmb_sig_t;
+
+    std::string __calc_lmb_sig(std::shared_ptr<code_lmb_t> lmb) {
+
+        std::stringstream ss;
+
+        for (auto &inst : lmb->body.insts) {
+            if (inst.type == code_inst_t::APPLY) {
+                ss << "A" << inst.retv << inst.func << inst.arg;
+            } else {
+                ss << "L" << inst.retv << inst.lmb->name;
+                for (auto env : inst.envs)
+                    ss << env;
+            }
+        }
+        ss << "R" << lmb->body.retv;
+
+        return ss.str();
+    }
+
+    std::vector<int> __relabel_lmb(std::shared_ptr<code_lmb_t> lmb, std::map<code_id_t, code_lmb_ref_t> &lmb_remap) {
+
+        std::vector<int> env_map;
+        std::map<code_id_t, code_id_t> ident_map;
+        int next_local_id = 0, next_env_id = 0;
+
+        lmb->body.deps.clear();
+
+        auto remap_id = [&](code_id_t id) {
+            switch (id.type) {
+                case code_id_t::ARG:
+                    return id;
+                case code_id_t::GLOBAL:
+                    if (builtin_ids.count(id))
+                        return id;
+                    lmb->body.deps.insert(lmb_remap[id].first);
+                    return lmb_remap[id].first->name;
+                case code_id_t::LOCAL:
+                    if (!ident_map.count(id))
+                        return ident_map[id] = local_id(next_local_id++);
+                    else
+                        return ident_map[id];
+                case code_id_t::ENV:
+                    if (!ident_map.count(id)) {
+                        env_map.push_back(id.val);
+                        return ident_map[id] = env_id(next_env_id++);
+                    } else {
+                        return ident_map[id];
+                    }
+                default:
+                    assert(false);
+            }
+        };
+
+        for (auto &inst : lmb->body.insts) {
+            inst.retv = remap_id(inst.retv);
+            if (inst.type == code_inst_t::APPLY) {
+                inst.func = remap_id(inst.func);
+                inst.arg = remap_id(inst.arg);
+            } else {
+                auto &pair = lmb_remap[inst.lmb->name];
+                std::vector<code_id_t> nenvs;
+                for (auto idx : pair.second)
+                    nenvs.push_back(remap_id(inst.envs[idx]));
+                inst.lmb = pair.first;
+                inst.envs = nenvs;
+                lmb->body.deps.insert(inst.lmb);
+            }
+        }
+        lmb->body.retv = remap_id(lmb->body.retv);
+
+        assert((int)env_map.size() == lmb->env_cnt);
+        return env_map;
+    }
+
+    void __do_dedup_lmb(
+        std::shared_ptr<code_lmb_t> lmb,
+        std::map<std::string, std::shared_ptr<code_lmb_t>> &sig_to_lmb,
+        std::map<code_id_t, code_lmb_ref_t> &lmb_remap
+    ) {
+        for (auto dep : lmb->body.deps)
+            if (!lmb_remap.count(dep->name))
+                __do_dedup_lmb(dep, sig_to_lmb, lmb_remap);
+
+        // do remap
+        std::vector<int> env_ord = __relabel_lmb(lmb, lmb_remap);
+        std::string lmb_sig = __calc_lmb_sig(lmb);
+        auto it = sig_to_lmb.find(lmb_sig);
+
+        if (it != sig_to_lmb.end()) {
+            std::vector<int> env_map(lmb->env_cnt);
+            lmb_remap[lmb->name] = code_lmb_ref_t{it->second, env_ord};
+        } else {
+            std::vector<int> rev_map(lmb->env_cnt);
+            for (int i = 0; i < lmb->env_cnt; i++)
+                rev_map[env_ord[i]] = i;
+            sig_to_lmb[lmb_sig] = lmb;
+            lmb_remap[lmb->name] = code_lmb_ref_t{lmb, env_ord};
+        }
+    }
+
+    void __dedup_lmb(std::shared_ptr<code_lmb_t> lmb) {
+        std::map<std::string, std::shared_ptr<code_lmb_t>> sig_to_lmb;
+        std::map<code_id_t, code_lmb_ref_t> lmb_remap;
+        __do_dedup_lmb(lmb, sig_to_lmb, lmb_remap);
     }
 };
 
